123f8465f1e45ff2f3d6138897e14a6e59f787a5
Yash Thakkar <thakkaryash94@gmail.com>
2018-08-23 00:31:55 +0530

    updated minor changes

--- index.js
+++ index.js
@@ -20,65 +20,58 @@ MongoClient.connect("mongodb://localhost:27017,localhost:27018,localhost:27019?r
     // specify db and collections
     const db = client.db("superheroesdb");
     const collection = db.collection("superheroes");
-    const changeStream = collection.watch(pipeline);
 
-    changeStream.on("change", function(change) {
+    const changeStream = collection.watch(pipeline);
+    // start listen to changes
+    changeStream.on("change", function (change) {
       console.log(change);
     });
 
-    /*
-    We will timeout function so that we can see what is actually happening in slow motion.
-    */
-
-    // insert few document
-    setTimeout(function() {
-      collection.insert({ "batman": "bruce wayne" }, function(err) {
+    // insert few data with timeout so that we can watch it happening
+    setTimeout(function () {
+      collection.insertOne({ "batman": "bruce wayne" }, function (err) {
         assert.ifError(err);
       });
     }, 1000);
-
-    setTimeout(function() {
-      collection.insert({ "superman": "clark kent" }, function(err) {
+    
+    setTimeout(function () {
+      collection.insertOne({ "superman": "clark kent" }, function (err) {
         assert.ifError(err);
       });
     }, 2000);
-
-    setTimeout(function() {
-      collection.insert({ "wonder-woman": "diana prince" }, function(err) {
+    
+    setTimeout(function () {
+      collection.insertOne({ "wonder-woman": "diana prince" }, function (err) {
         assert.ifError(err);
       });
     }, 3000);
-
-    setTimeout(function() {
-      collection.insert({ "ironman": "tony stark" }, function(err) {
+    
+    setTimeout(function () {
+      collection.insertOne({ "ironman": "tony stark" }, function (err) {
         assert.ifError(err);
       });
     }, 4000);
-
-    setTimeout(function() {
-      collection.insert({ "spiderman": "peter parker" }, function(err) {
+    
+    setTimeout(function () {
+      collection.insertOne({ "spiderman": "peter parker" }, function (err) {
         assert.ifError(err);
       });
     }, 5000);
-
+    
     // update existing document
-    setTimeout(function() {
-      collection.updateOne(
-        { "ironman": "tony stark" },
-        { $set: { "ironman": "elon musk" } },
-        function(err) {
-          assert.ifError(err);
-        }
-      );
+    setTimeout(function () {
+      collection.updateOne({ "ironman": "tony stark" }, { $set: { "ironman": "elon musk" } }, function (err) {
+        assert.ifError(err);
+      });
     }, 6000);
-
+    
     // delete existing document
-    setTimeout(function() {
-      collection.deleteOne({ "spiderman": "peter parker" }, function(err) {
+    setTimeout(function () {
+      collection.deleteOne({ "spiderman": "peter parker" }, function (err) {
         assert.ifError(err);
       });
     }, 7000);
   })
   .catch(err => {
-  console.error(err);
-});
+    console.error(err);
+  });
